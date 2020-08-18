256ab5e7d3dac49ffbb2e577896d0796fbfd2f8e
Akhil <legitthug54@gmail.com>
2020-08-18 01:30:15 -0400

    fixed api query for action movies, add gif in readme

--- src/requests.js
+++ src/requests.js
@@ -4,7 +4,7 @@ const requests = {
   fetchTrending: `trending/all/day?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=20`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
