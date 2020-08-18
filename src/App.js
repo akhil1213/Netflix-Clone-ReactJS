9a2c6df7817afe2f697496b51cc87c9dccf4db1e
akhil1213 <legitthug54@gmail.com>
2020-08-17 23:13:16 -0400

    using lots of flexbox navbar background color change to black on scroll needs work

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
-// import Nav from "./Nav";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="app">
-      {/* <Nav /> */}
+      <Nav />
       <Banner />
       <Row
         isLargeRow
